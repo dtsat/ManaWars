130fae8f7a493a2d847224a19aaef699fe05628f
Philip-Eloy <philipeloy@gmail.com>
2017-04-03 19:10:47 -0400

    Basic Shielding done!
    Every 6 seconds, the fairy will spawn a shield in front of the player.
    
    TODO:
    Fairy animaition when casting
    Distance requirement so that the fairy has to be near you to cast
    Shield.

---
+++ Assets/Scripts/FairyMagic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FairyMagic : MonoBehaviour {
+
+	public GameObject Wizard;
+	public Vector3 wizPosition;
+	public Quaternion wizRotation;
+	float distFromWiz;
+	public GameObject Shield;
+	public Vector3 offsetShield;
+
+	public float fairyshieldtimer = 6f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		fairyshieldtimer -= (Time.deltaTime) * 1;
+
+		if (fairyshieldtimer <= 0) {
+			wizPosition = new Vector3 (Wizard.transform.position.x, (Wizard.transform.position.y + 1), (Wizard.transform.position.z));
+			wizRotation = Quaternion.LookRotation (Wizard.transform.position, Vector3.up);
+			Shield.transform.position = wizPosition;
+			offsetShield = new Vector3(Shield.transform.position.x, Shield.transform.position.y + 0.5f, Shield.transform.position.z + 2);
+			Shield.transform.rotation = Quaternion.RotateTowards (transform.rotation, wizRotation, 999999f);
+			Shield.transform.position = offsetShield;
+			Instantiate (Shield);
+			fairyshieldtimer = 6;
+		}
+
+	}
+}
---
+++ Assets/Scripts/FairyShield.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FairyShield : MonoBehaviour {
+
+	public float lifetime = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifetime -= (Time.deltaTime) * 2;
+
+		if (lifetime <= 0) {
+			GameObject.Destroy (gameObject);
+		}
+	}
+}
