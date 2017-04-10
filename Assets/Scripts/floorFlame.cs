9701420678875b173fd69fb5b89f4b5257c10e98
Jonathan Bobak <jon_bobak89@hotmail.com>
2017-04-09 20:33:19 -0400

    Flames for ground

---
+++ Assets/Scripts/floorFlame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floorFlame : MonoBehaviour {
+    float destroyTimer = 3f;
+	// Use this for initialization
+	void Awake () {
+        Destroy(this.gameObject, destroyTimer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
