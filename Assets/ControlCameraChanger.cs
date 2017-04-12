f29c48cc519830bf33103d24536f50f738d32518
Philip-Eloy <philipeloy@gmail.com>
2017-04-12 14:45:20 -0400

    Insctruction screen improved
    now with Lore.

---
+++ Assets/ControlCameraChanger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlCameraChanger : MonoBehaviour {
+
+	public GameObject IntroCam;
+	public GameObject SmashCam;
+	public GameObject ControlCam;
+
+	// Use this for initialization
+	void Start () {
+		IntroCam = GameObject.Find ("IntroCam");
+		SmashCam = GameObject.Find ("SmashCamera");
+		ControlCam = GameObject.Find ("Controls Camera");
+	}
+
+	public void ToSmash()
+	{
+		Debug.Log ("WTF!");
+		SmashCam.GetComponent<Camera> ().enabled = true;
+		IntroCam.GetComponent<Camera> ().enabled = false;
+	}
+
+	public void ToControls()
+	{
+		ControlCam.GetComponent<Camera> ().enabled = true;
+		SmashCam.GetComponent<Camera> ().enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
