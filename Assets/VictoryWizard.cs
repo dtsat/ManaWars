7076efccacb12f4fe79246bede600a55ceb1ed52
Philip-Eloy <philipeloy@gmail.com>
2017-04-12 23:18:19 -0400

    Victory screen
    Jump up and down on a Treant, hurray.
    
    We don't have a Victory picture, so I just used the logo.

---
+++ Assets/VictoryWizard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryWizard : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetTrigger("Victory");
+	}
+}
